317a43312cb76b2463984919e4b88dab8cb8b2cc
SJ <shevajia@gmail.com>
2015-02-04 17:05:22 -0600

    modifying parse.py

--- assignment4/parse.py
+++ assignment4/parse.py
@@ -14,3 +14,19 @@ def readCSV(filename):
 
 
 ### enter your code below
+data = readCSV('permits_hydepark.csv')
+def get_avg_latlng(arg):
+    '''computes the average latitude and longitude of construction permits in Hyde Park and prints it to the console'''
+    lattitudes = []
+    longtitudes = []
+    for i in range(len(arg)):
+        lattitudes.append(arg[i][-3])
+        longtitudes.append(arg[i][-2])
+    lattitudes_total  = sum([float(n) if n else 0 for n in lattitudes])
+    lattitudes_average = lattitudes_total/len(lattitudes)
+    longtitudes_total = sum([float(m) if m else 0 for m in longtitudes])
+    longtitudes_average = longtitudes_total/len(longtitudes)
+    return "The average lattitude is " + str(lattitudes_average) + ", and the average longtitude is " + str(longtitudes_average) +"."
+
+
+print get_avg_latlng(data)
