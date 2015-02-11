aad37fca2cc176a04ee6b8322deb01e1688e6506
Shilin Jia <shevajia@gmail.com>
2015-02-11 00:49:08 -0600

    updating main.py

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -7,6 +7,22 @@ from barChart import plotBarChart
 if __name__ == '__main__':
     artist_names = sys.argv[1:]
     print "input artists are ", artist_names
-    # YOUR CODE HERE
+    artist_info_list = []
+    album_info_list = []
+    for artist_name in artist_names: 	
+    	artist_id = fetchArtistId(artist_name)
+    	artist_info = fetchArtistInfo(artist_id)
+        artist_info_list.append(artist_info)
+        albums_id = fetchAlbumIds(artist_id)
+        for album_id in albums_id:
+        	album_info = fetchAlbumInfo(album_id)
+        	album_info_list.append(album_info)
+
+    writeArtistsTable(artist_info_list)
+    writeAlbumsTable(album_info_list)
+    plotBarChart()
+
+
+
     
 
