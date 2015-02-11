554ddc03c188b930da053fefb3f6fd3d524f9881
SJ <shevajia@gmail.com>
2015-02-11 14:45:22 -0600

    updating csvUtil.py

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -7,10 +7,10 @@ def writeArtistsTable(artist_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY
     """
-    outfile = open('artists.csv', 'w')
+    outfile = open('artists.csv', 'w', encoding = 'utf-8')
     outfile.write(u'ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY\n')
     for artist_info in artist_info_list:
-        line = artist_info['id']+ u',"' + artist_info['name'] + u'",' + artist_info['followers'] + u',' + artist_info['popularity'] + u'\n'
+        line = artist_info['id']+ u',"' + artist_info['name'] + u'",' + str(artist_info['followers']) + u',' + str(artist_info['popularity']) + u'\n'
         outfile.write(line) 
     outfile.close()   
 
@@ -23,9 +23,9 @@ def writeAlbumsTable(album_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
-    outfile = open('albums.csv', 'w')
+    outfile = open('albums.csv', 'w', encoding = 'utf-8')
     outfile.write(u'ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY\n')
     for album_info in album_info_list:
-        line = album_info['artist_id']+ u',' + album_info['album_id'] + u',"' + album_info['name'] + u'",' + album_info['year'] + u',' + album_info['popularity'] + u'\n'
+        line = album_info['artist_id']+ u',' + album_info['album_id'] + u',"' + album_info['name'] + u'",' + album_info['year'] + u',' + str(album_info['popularity']) + u'\n'
         outfile.write(line) 
     outfile.close() 
